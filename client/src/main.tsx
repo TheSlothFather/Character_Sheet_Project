ee57f51a1b79643c1a5b73a8a8dcdc6be07bdc86
TheSlothFather <130124752+TheSlothFather@users.noreply.github.com>
2025-12-07 15:58:54 -0600

    Wrap app with definitions provider in main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./modules/App";
+import { DefinitionsProvider } from "./modules/definitions/DefinitionsProvider";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <DefinitionsProvider>
+      <App />
+    </DefinitionsProvider>
   </React.StrictMode>
 );
 
